d59459745d89b161a6dc9c9cdaee3de9ccacd8b4
anju-r <anjalirallabandi@gmail.com>
2024-02-08 14:47:19 -0600

    Start back-end folder and add all dependancies

---
+++ aurora-backend/aurora-hotel/src/main/java/com/anjali/aurorahotel/AuroraHotelApplication.java
@@ -0,0 +1,13 @@
+package com.anjali.aurorahotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuroraHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuroraHotelApplication.class, args);
+	}
+
+}
---
+++ aurora-backend/aurora-hotel/src/test/java/com/anjali/aurorahotel/AuroraHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anjali.aurorahotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuroraHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
