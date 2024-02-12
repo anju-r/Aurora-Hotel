cac6b6604740154bf6aeb948d9dc8810c3228207
anju-r <anjalirallabandi@gmail.com>
2024-02-12 14:12:22 -0600

    Create tables

--- aurora-backend/aurora-hotel/src/main/java/com/anjali/aurorahotel/model/BookedRoom.java
+++ aurora-backend/aurora-hotel/src/main/java/com/anjali/aurorahotel/model/BookedRoom.java
@@ -1,4 +1,17 @@
 package com.anjali.aurorahotel.model;
 
+import java.time.LocalDate;
+
 public class BookedRoom {
+    private Long bookingId;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private String guestFullName;
+    private String guestEmail;
+    private int NumOfAdults;
+    private int NumOfChildren;
+    private int totalNumOfGuest;
+    private String bookingConfirmationCode;
+    private Room room;
+
 }
