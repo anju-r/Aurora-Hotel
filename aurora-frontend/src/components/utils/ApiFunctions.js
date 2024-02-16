5e700592783611fb946c52d9db5dbd987abb3eed
anju-r <anjalirallabandi@gmail.com>
2024-02-16 14:17:58 -0600

    Work on code to add new room from front end

--- aurora-frontend/src/components/utils/ApiFunctions.js
+++ aurora-frontend/src/components/utils/ApiFunctions.js
@@ -3,7 +3,7 @@ import axios from axios;
 export const api = axios.create({
     baseURL: "http://localhost:8080"
 })
-
+//This function adds new room to the database
 export async function addRoom(photo, roomType, roomPrice) {
     const formData = new FormData()
     formData.append("photo", photo)
@@ -15,5 +15,15 @@ export async function addRoom(photo, roomType, roomPrice) {
         return true
     } else {
         return false
+    }
+}
+
+/*This function fetches all the room types from database*/
+export async function getRoomTypes() {
+    try {
+        const response = await api.get("/rooms/room-types")
+        return response.data
+    } catch (error) {
+        throw new error("Error fetching room types")
     }
 }
