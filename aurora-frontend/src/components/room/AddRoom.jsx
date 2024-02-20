86b91d771f19e5ad24896dba087ac38ff7bf4efa
anju-r <anjalirallabandi@gmail.com>
2024-02-19 23:52:00 -0600

    Work on code to add new room from front end

--- aurora-frontend/src/components/room/AddRoom.jsx
+++ aurora-frontend/src/components/room/AddRoom.jsx
@@ -72,10 +72,32 @@ const AddRoom = () => {
                   className="form-control"
                   required
                   id="roomPrice"
+                  type="number"
                   name="roomPrice"
                   value={newRoom.roomPrice}
                   onChange={handleRoomInputChange}
                 />
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="photo" className="form-label">
+                  Room Photo
+                </label>
+                <input
+                  className="form-control"
+                  required
+                  id="roomPrice"
+                  name="photo"
+                  type="file"
+                  value={newRoom.roomPrice}
+                  onChange={handleImageChange}
+                />
+                {imagePreview && (
+                  <img src={imagePreview}
+                    alt="Preview room photo"
+                    style={{ maxWidth: "400px", maxHeight: "400px" }}
+                    className="mb-3"
+                )}
               </div>
 
 
