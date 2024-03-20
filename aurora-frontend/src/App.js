ceecc15fddcb38f3ad7345c1379e02ff74803550
anju-r <anjalirallabandi@gmail.com>
2024-03-20 16:55:02 -0500

    Update room from front-end

--- aurora-frontend/src/App.js
+++ aurora-frontend/src/App.js
@@ -3,13 +3,29 @@ import './App.css';
 //import "../node_modules/bootsrap/dist/css/bootsrap.min.css"
 //import "/node_modules/bootsrap/dist/js/bootsrap.min.js"
 import AddRoom from './components/room/AddRoom';
+import ExistingRooms from './components/room/ExistingRooms';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import EditRoom from './components/room/EditRoom';
+import Home from './components/home/Home';
 
 function App() {
   return (
     <>
-      <AddRoom />
+      <main>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/edit-room/:roomId" element={<EditRoom />} />
+            <Route path="/existing-rooms" element={<ExistingRooms />} />
+
+
+
+          </Routes>
+        </Router>
+      </main>
+
     </>
-  );
+  )
 
 }
 
