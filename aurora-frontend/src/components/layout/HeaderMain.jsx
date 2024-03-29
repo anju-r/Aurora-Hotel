86e31a6e44264c79201a9227145b946869353489
anju-r <anjalirallabandi@gmail.com>
2024-03-29 16:33:23 -0500

    Create Header and parallax in homepage

--- aurora-frontend/src/components/layout/HeaderMain.jsx
+++ aurora-frontend/src/components/layout/HeaderMain.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const HeaderMain = () => {
     return (
-        <div>HeaderMain</div>
+        <header className="header-banner">
+            <div className="overlay"></div>
+            <div className="animated-texts overlay-content">
+                <h1>
+                    Welcome to <span className="hotel-color"> Aurora Hotel</span>
+                </h1>
+                <h4>Experience The Best Hospitality In Town</h4>
+            </div>
+        </header>
     )
 }
 
