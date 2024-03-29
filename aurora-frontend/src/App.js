813b43668e29705263ed452b284a569f0fc8a217
anju-r <anjalirallabandi@gmail.com>
2024-03-29 14:49:16 -0500

    Create Admin page and roomlistings

--- aurora-frontend/src/App.js
+++ aurora-frontend/src/App.js
@@ -9,6 +9,7 @@ import EditRoom from './components/room/EditRoom';
 import Home from './components/home/Home';
 import { Navbar } from 'react-bootstrap';
 import Footer from './components/layout/Footer';
+import RoomListing from './components/room/RoomListing';
 
 function App() {
   return (
@@ -21,6 +22,8 @@ function App() {
             <Route path="/edit-room/:roomId" element={<EditRoom />} />
             <Route path="/existing-rooms" element={<ExistingRooms />} />
             <Route path="/add-room" element={<AddRoom />} />
+            <Route path="/browse-all-rooms" element={<RoomListing />} />
+            <Route path="/admin" element={<Admin />} />
 
 
 
