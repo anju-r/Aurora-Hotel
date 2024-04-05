95a1e785e4efc7400b39052e808a189674fa4cf9
anju-r <anjalirallabandi@gmail.com>
2024-04-05 15:12:19 -0500

    Finish carousel page display

--- aurora-frontend/src/components/common/RoomCarousel.jsx
+++ aurora-frontend/src/components/common/RoomCarousel.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { getAllRooms } from '../utils/ApiFunctions'
+import { Card, Carousel, Container, Row } from 'react-bootstrap'
 
 const RoomCarousel = () => {
-  const [rooms, setRooms] = useState([])
+  const [rooms, setRooms] = useState([{ id: "", roomType: "", roomPrice: "", photo: "" }])
   const [errorMessage, setErrorMessage] = useState("")
   const [isLoading, setIsLoading] = useState(false)
 
@@ -16,8 +17,54 @@ const RoomCarousel = () => {
       setIsLoading(false)
     })
   }, [])
+  if (isLoading) {
+    return <div className="mt-5">Loading Rooms....</div>
+  }
+  if (errorMessage) {
+    return <div className="text-danger mb-5 mt-5">Erroor : {errorMessage}</div>
+  }
   return (
-    <div></div>
+    <section className="bg-light mb-5 shadow">
+      <Link to={"/browse-all-rooms"} className="hotel-color text-center">
+        Browse all rooms
+      </Link>
+      <Container>
+        <Carousel indicators={false}>
+          {[...Array(Math.ceil(rooms.length / 4))].map((_, index) => (
+            <Carousel.Item key={index}>
+              <Row>
+                {rooms.slice(index * 4, index * 4 + 4).map((room) => (
+                  <Col key={room.id} className="mb-4" xs={12} md={6} lg={3}>
+                    <Card>
+                      <Link to={`/book-room/${room.id}`}>
+                        <Card.Img
+                          variant="top"
+                          src={`data:image/png;base64, ${room.photo}`}
+                          alt="Room Photo"
+                          className="w-100"
+                          style={{ height: "200px" }}
+                        />
+                      </Link>
+                      <Card.Body>
+                        <Card.Title className="hotel-color">{room.roomType}</Card.Title>
+                        <Card.Title className="room-price">${room.roomPrice}/night</Card.Title>
+                        <div className="flex-shrink-0">
+                          <Link to={`/book-room/${room.id}`} className="btn btn-hotel btn-sm">
+                            Book Now
+                          </Link>
+                        </div>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                ))}
+              </Row>
+            </Carousel.Item>
+          ))}
+
+        </Carousel>
+      </Container>
+
+    </section>
   )
 }
 
