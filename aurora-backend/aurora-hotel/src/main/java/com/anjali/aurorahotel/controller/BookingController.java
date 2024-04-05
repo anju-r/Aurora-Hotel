f7f10783f52f79871e33a6778aafc93e0881be4a
anju-r <anjalirallabandi@gmail.com>
2024-04-05 16:20:17 -0500

    write backend code to list rooms

---
+++ aurora-backend/aurora-hotel/src/main/java/com/anjali/aurorahotel/controller/BookingController.java
@@ -0,0 +1,44 @@
+package com.anjali.aurorahotel.controller;
+import com.anjali.aurorahotel.exception.ResourceNotFoundException;
+import com.anjali.aurorahotel.model.BookedRoom;
+import com.anjali.aurorahotel.response.BookingResponse;
+import com.anjali.aurorahotel.service.IBookingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/bookings")
+public class BookingController {
+    private final IBookingService bookingService;
+
+    @GetMapping("all-bookings")
+    public ResponseEntity<List<BookingResponse>> getAllBookings(){
+        List<BookedRoom> bookings = bookingService.getAllBookings();
+        List<BookingResponse> bookingResponses = new ArrayList<>();
+        for (BookedRoom booking : bookings){
+            BookingResponse bookingResponse = getBookingResponse(booking);
+            bookingResponses.add(bookingResponse);
+        }
+        return ResponseEntity.ok(bookingResponses);
+    }
+
+    @GetMapping("/confirmation/{confirmationCode}")
+    public ResponseEntity<?> getBookingConfirmationCode(@PathVariable String confirmationCode){
+        try {
+            BookedRoom booking = bookingService.findByBookingConfirmationCode(confirmationCode);
+            BookingResponse bookingResponse = getBookingResponse(booking);
+            return new ResponseEntity.ok(bookingResponse);
+        }catch (ResourceNotFoundException ex){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+        }
+    }
+}
