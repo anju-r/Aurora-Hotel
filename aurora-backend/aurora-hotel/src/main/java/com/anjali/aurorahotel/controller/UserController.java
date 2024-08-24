e6e211e8611505b257ff29f17d9216d404e9acc4
anju-r <anjalirallabandi@gmail.com>
2024-08-23 22:27:31 -0500

    Add User Controller

---
+++ aurora-backend/aurora-hotel/src/main/java/com/anjali/aurorahotel/controller/UserController.java
@@ -0,0 +1,44 @@
+package com.dailycodework.lakesidehotel.controller;
+
+import com.dailycodework.lakesidehotel.model.User;
+import com.dailycodework.lakesidehotel.service.IUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @author Simpson Alfred
+ */
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final IUserService userService;
+
+    @GetMapping("/all")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<List<User>> getUsers(){
+
+        return new ResponseEntity<>(userService.getUsers(), HttpStatus.FOUND);
+    }
+
+    @GetMapping("/{email}")
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+    public ResponseEntity<?> getUserByEmail(@PathVariable("email") String email){
+        try{
+            User theUser = userService.getUser(email);
+            return ResponseEntity.ok(theUser);
+        }catch (UsernameNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error fetching user");
+        }
+    }
