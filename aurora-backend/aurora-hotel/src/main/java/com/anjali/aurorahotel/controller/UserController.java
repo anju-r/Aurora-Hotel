7184fb181603d9cd1d8457229077a35381709e94
anju-r <anjalirallabandi@gmail.com>
2024-08-29 23:15:30 -0500

    Add delete user in user controller

--- aurora-backend/aurora-hotel/src/main/java/com/anjali/aurorahotel/controller/UserController.java
+++ aurora-backend/aurora-hotel/src/main/java/com/anjali/aurorahotel/controller/UserController.java
@@ -42,3 +42,16 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error fetching user");
         }
     }
+    @DeleteMapping("/delete/{userId}")
+    @PreAuthorize("hasRole('ROLE_ADMIN') or (hasRole('ROLE_USER') and #email == principal.username)")
+    public ResponseEntity<String> deleteUser(@PathVariable("userId") String email){
+        try{
+            userService.deleteUser(email);
+            return ResponseEntity.ok("User deleted successfully");
+        }catch (UsernameNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error deleting user: " + e.getMessage());
+        }
+    }
+}
